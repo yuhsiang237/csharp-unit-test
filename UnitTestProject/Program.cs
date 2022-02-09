df06a4f6a92f8824169e106587faf61dfcead9aa
yuhsiang237 <umworksite@gmail.com>
2022-02-10 05:55:26 +0800

    init unit test project

---
+++ UnitTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnitTestProjectTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+
+using Xunit;
+using Moq;
+
+namespace UnitTestProjectTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           
+        }
+    }
+}
